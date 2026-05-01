f4968ba5a86c34bec675aad49b56d8ab7d01338f
saraotb390-hash <saraotb390@gmail.com>
2026-05-01 09:24:50 +0300

    Update projects.py

--- projects.py
+++ projects.py
@@ -140,9 +140,9 @@ try:
     )
     client = gspread.authorize(creds)
     sheet = client.open_by_url("https://docs.google.com/spreadsheets/d/1rh_qlc52ytF5vntax0iRL6vkmZzGKt37MC_L2_iaojc/edit?gid=0#gid=0").sheet1
-    sheet.append_row(['test'])
+    sheet.append_row([result])
 except Exception as e:
-    st.write("خطأ:", e)
+    st.write("وصلت هنا")
     st.write("خطأ في Google Sheets:", e)
     # ===== الوصف =====
 descriptions = {

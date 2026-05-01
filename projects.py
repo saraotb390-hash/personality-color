edcfefacd4dbd2e6757d02736dac66e1cc0af8fc
saraotb390-hash <saraotb390@gmail.com>
2026-05-01 08:12:41 +0300

    Update projects.py

--- projects.py
+++ projects.py
@@ -140,11 +140,11 @@ try :
       creds_dict,
       scope
     )
-st.write('وصلت هنا')
+
     client = gspread.authorize(creds)
     sheet = client.open_by_url("https://docs.google.com/spreadsheets/d/1rh_qlc52ytF5vntax0iRL6vkmZzGKt37MC_L2_iaojc/edit?gid=0#gid=0").sheet1
 
-    sheet.append_row([test])
+    sheet.append_row(['test'])
 except Exception as e:
 
     st.write("خطأ في Google Sheets:", e)

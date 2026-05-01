74d69ac9b78e4da68b006b58c41cfb2104db79ef
saraotb390-hash <saraotb390@gmail.com>
2026-05-01 06:46:23 +0300

    Update projects.py

--- projects.py
+++ projects.py
@@ -257,34 +257,3 @@ st.markdown("""
     """, unsafe_allow_html=True)
 
 # ===============================
-# 📊 البروجكتر
-# ===============================
-
-
-    st.title("📊 توزيع الألوان")
-
-if os.path.exists(file):
-    df = pd.read_csv(file)
-
-if not df.empty:
-
-    chart = df["color"].value_counts()
-
-    colors_map = {
-        "الأحمر": "red",
-        "الأزرق": "blue",
-        "الأخضر": "green",
-        "الأصفر": "yellow"
-    }
-
-    colors = [colors_map.get(c, "gray") for c in chart.index]
-    labels = [get_display(arabic_reshaper.reshape(label)) for label in chart.index]
-
-    fig, ax = plt.subplots()
-    ax.bar(labels, chart.values, color=colors)
-
-    st.pyplot(fig)
-
-else:
-    st.write("لا توجد بيانات بعد")
-

a21a874ac00e94e2eb7027441c47112362473bd9
saraotb390-hash <saraotb390@gmail.com>
2026-05-01 12:10:22 +0300

    Update projects.py

--- projects.py
+++ projects.py
@@ -133,11 +133,10 @@ if show:
 try:
     scope = ["https://spreadsheets.google.com/feeds",
      "https://www.googleapis.com/auth/drive"]
-    creds_dict = st.secrets["gcp_service_account"]
-    creds = ServiceAccountCredentials.from_json_keyfile_dict(
-    creds_dict,
+    creds = ServiceAccountCredentials.from_json_keyfile_name(
+    "credentials.json",
     scope
-    )
+    ) 
     client = gspread.authorize(creds)
     sheet = client.open_by_url("https://docs.google.com/spreadsheets/d/1rh_qlc52ytF5vntax0iRL6vkmZzGKt37MC_L2_iaojc/edit?gid=0#gid=0").sheet1
     sheet.append_row([result])
